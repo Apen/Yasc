5de3f1148fad4bb862a04c2325361e8a00b34636
Yohann CERDAN <cerdanyohann@yahoo.fr>
2014-08-05 12:21:10 +0200

    [FEATURE] Add Match class to match a regexp on a page and store results in file

---
+++ Lib/Match.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Yasc;
+
+/**
+ * Class match
+ *
+ * @package Yasc
+ */
+class Match {
+
+	/** @var \Yasc\Config $config */
+	private $config;
+
+	/**
+	 * @param array $config
+	 */
+	public function __construct($config) {
+		$this->config = $config;
+	}
+
+
+	/**
+	 * Exec preg_match on teh response
+	 *
+	 * @param string $url
+	 * @param array  $response
+	 */
+	public function matchValues($url, $response) {
+		$matchValues = $this->config->getMatchValues();
+		//\Yasc\Log::write($matchValues);
+		foreach ($matchValues as $matchValue) {
+			if (preg_match_all('#' . $matchValue . '#', $response['data'], $matches) > 0) {
+				$this->writeMatchValues($matchValue, $url);
+			}
+		}
+	}
+
+	protected function writeMatchValues($matchValue, $url) {
+		// write txt file
+		$handle = fopen($this->getMatchValuesTxtFile(), 'a');
+		if (!$handle) {
+			die("Can't open file " . $this->getMatchValuesTxtFile());
+		}
+		fwrite($handle, $matchValue . ' --> ' . $url . PHP_EOL);
+		fclose($handle);
+	}
+
+	/**
+	 * Get match values txt file
+	 *
+	 * @return string
+	 */
+	protected function getMatchValuesTxtFile() {
+		return realpath(__DIR__ . '/../Crawl') . '/match.txt';
+	}
+
+}
